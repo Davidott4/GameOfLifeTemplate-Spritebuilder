a850f92f1739b4c5ce8b5d1c556c50e40f222b0d
dott <dott@mail.ccsf.edu>
2014-06-24 11:25:36 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -51,11 +51,27 @@ static const int GRID_COLUMNS = 10;
             [self addChild:creature];
             
             _gridArray[i][j] = creature;
-            creature.isAlive = YES;
+            creature.isAlive = NO;
             
             x+=_cellWidth;
         }
         y+=_cellHeight;
     }
 }
+
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    CGPoint touchLocation = [touch locationInNode:self];
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    creature.isAlive =!creature.isAlive;
+}
+
+-(Creature *) creatureForTouchPosition:(CGPoint) touchPosition
+{
+    NSInteger row = touchPosition.y / (_cellHeight);
+    NSInteger column = touchPosition.x / (_cellWidth);
+    
+    return _gridArray[row][column];
+}
+
 @end
